dc1a8db4c8ee4006725b01e1588d7420545d68f7
JoshuaSMoore <J_Moore23@hotmail.com>
2021-10-01 16:26:53 -0600

    Bug service side

---
+++ BugCatcher/server/controllers/BugsController.js
@@ -0,0 +1,90 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { logger } from '../utils/Logger.js'
+import BaseController from '../utils/BaseController.js'
+import { bugsService } from '../services/BugsService.js'
+import { dbContext } from '../db/DbContext.js'
+
+export class BugsController extends BaseController {
+  constructor() {
+    super('api/bugs')
+    this.router
+      .get('', this.getBugs)
+      .get('/:id', this.getBugById)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createBug)
+      .get(':/:id/notes', this.getNotesById)
+      .get('/:id/trackedbugs', this.getTrackedBugs)
+      .put('/:id', this.editBug)
+      .delete('/:id', this.deleteBug)
+  }
+
+  async getBugs(req, res, next) {
+    try {
+      const bugs = await bugsService.getBugs(req.query)
+      res.send(bugs)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getBugById(req, res, next) {
+    try {
+      const bug = await bugsService.getBugById(req.params.bugId)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createBug(req, res, next) {
+    try {
+      logger.log('who is the current?', req.userInfo)
+      // REVIEW IMPORTANT never ever ever ever trust the client
+      // force the creatorId to be the loggedin user
+      // if (!req.userInfo.email_verified) {
+      //   throw new UnAuthorized('Please validate your email')
+      // }
+      req.body.creatorId = req.userInfo.id
+      const bug = await bugsService.createBug(req.body)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getNotesById(req, res, next) {
+    try {
+      const notes = await bugsService.getNotesById(req.params.bugId)
+      res.send(notes)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editBug(req, res, next) {
+    try {
+      const bug = await bugsService.editBug(req.params.bugId, req.userInfo.id, req.body)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getTrackedBugs(req, res, next) {
+    try {
+      const bugs = await bugsService.getTrackedBugsById(req.params.bugId)
+      res.send(bugs)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteBug(req, res, next) {
+    try {
+      const bug = await bugsService.closeBug(req.params.bugId, req.params.userId)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
---
+++ BugCatcher/server/services/BugsService.js
@@ -0,0 +1,56 @@
+import { dbContext } from '../db/DbContext.js'
+import { BadRequest, Forbidden } from '../utils/Errors.js'
+import { logger } from '../utils/Logger.js'
+
+class BugsService {
+  async getBugs(query) {
+    const bugs = await dbContext.Bug.find(query).sort('-createdAt').populate('creator', 'name picture')
+    return bugs
+  }
+
+  async getBugById(bugId) {
+    const bug = await dbContext.Bug.findById(bugId).populate('creator', 'name picture')
+    if (!bug) {
+      throw new BadRequest('Invalid Bug Id')
+    }
+    return bug
+  }
+
+  async createBug(bugData) {
+    const bug = await dbContext.Bug.create(bugData)
+    return bug
+  }
+
+  async getNotesById(bugId) {
+    const bug = await dbContext.Note.findById(bugId).populate('creator', 'name picture')
+    if (!bug) {
+      throw new BadRequest('Invalid Bug Id')
+    }
+  }
+
+  async getTrackedBugsById(bugId) {
+    const bugs = await dbContext.TrackedBug.find({ bugId }).populate('account', 'name picture')
+    return bugs
+  }
+
+  async editBug(bugId, userId, bugData) {
+    const bug = await this.getBugById(bugId)
+    if (userId !== bug.creatorId.toString()) {
+      throw new Forbidden('Nope, not allowed')
+    }
+    bug.title = bugData.title || bug.title
+    bug.description = bugData.description || bug.description
+    bug.priority = bugData.priority || bug.priority
+  }
+
+  async closeBug(bugId, userId) {
+    const bug = await this.getBugById(bugId)
+    if (userId !== bug.creatorId.toString()) {
+      throw new Forbidden('Not allowed to close')
+    }
+    bug.closed = true
+    return bug
+  }
+}
+
+export const bugsService = new BugsService()
