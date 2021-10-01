0482dd334ee53b52194e873186f0a0424639a540
JoshuaSMoore <J_Moore23@hotmail.com>
2021-10-01 15:05:28 -0600

    DB

--- BugCatcher/server/db/DbContext.js
+++ BugCatcher/server/db/DbContext.js
@@ -1,11 +1,19 @@
-import mongoose from 'mongoose'
+import mongoose, { mongo } from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { BugSchema } from '../models/Bug.js'
+import { NoteSchema } from '../models/Note.js'
+import { TrackedBugSchema } from '../models/TrackedBug.js'
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+
+  Bug = mongoose.model('Bug', BugSchema);
+
+  TrackedBug = mongoose.model('TrackedBug', TrackedBugSchema);
+  Note = mongoose.model('Note', NoteSchema)
 }
 
 export const dbContext = new DbContext()
